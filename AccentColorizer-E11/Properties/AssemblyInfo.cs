7a89369585e670d92e9b6067bca1d862c4ebcdbe
krlvm <51774833+krlvm@users.noreply.github.com>
2023-04-23 14:39:39 +0300

    Bump version to 1.1.2

--- AccentColorizer-E11/Properties/AssemblyInfo.cs
+++ AccentColorizer-E11/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("krlvm")]
 [assembly: AssemblyProduct("AccentColorizer-E11")]
-[assembly: AssemblyCopyright("Copyright © krlvm 2022")]
+[assembly: AssemblyCopyright("Copyright © krlvm 2022-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.1.2.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
